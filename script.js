3ce93222d37c66bb0d218f1063a29356976a0443
whyiamavirgin <izmajlovtima@gmail.com>
2020-11-11 10:13:26 +0600

    доделал небольшой адаптив

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 $('#menu-checkbox').on('click', function () {
     var x = document.getElementById('logo');
+    var y = document.getElementById('main');
+    var z = document.getElementById('footer-navig');
     if ( $(this).is(':checked') ) {
         x.className += " invisible";
+        y.className += " invisible";
+        z.className += " invisible";
     } else {
         // checkbox unchecked 
         x.className = "logo";
+        y.className = "main";
+        z.className = "footer-nav disabled";
     }
 })
 
@@ -18,6 +24,10 @@ if($(window).width() <= 420) {
 const anchors = document.querySelectorAll('a[href*="#"]')
 for (let anchor of anchors) {
     anchor.addEventListener("click", function(event) {
+        var y = document.getElementById('main');
+        var z = document.getElementById('footer-navig');
+        y.className -= " invisible";
+        z.className = "footer-nav disabled";
         event.preventDefault();
         const blockID = anchor.getAttribute('href')
         document.querySelector('' + blockID).scrollIntoView({
